daf6c72058e94e65f3396f3b5a6fabf0273cdece
Rehaab Syed <rehaab.syed@student.curtin.edu.au>
2022-04-08 13:00:53 +0800

    Changed grid

--- Maze.py
+++ Maze.py
@@ -15,12 +15,12 @@ if __name__ == "__main__":
     grid = [
         [ WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL],
         [START, EMPTY,  WALL,  WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, WALL],
-        [ WALL, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, EMPTY,  WALL, WALL],
+        [ WALL, EMPTY, WALL, EMPTY, EMPTY, EMPTY,  WALL, EMPTY,  WALL, WALL],
         [ WALL,  WALL,  WALL,  WALL, EMPTY,  WALL, EMPTY,  WALL, EMPTY, WALL],
-        [ WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, EMPTY, EMPTY, EMPTY, WALL],
+        [ WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, WALL, EMPTY, EMPTY, WALL],
         [ WALL,  WALL, EMPTY,  WALL,  WALL, EMPTY, EMPTY,  WALL, EMPTY, WALL],
-        [ WALL,  WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL,  WALL, EMPTY,  END],
-        [ WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL],
+        [ WALL,  WALL, WALL, EMPTY, WALL, EMPTY,  WALL,  WALL, EMPTY,  END],
+        [ WALL,  WALL,  WALL,  EMPTY,  WALL,  EMPTY,  WALL,  WALL,  WALL, WALL],
     ]
                     
     Viewer.view(grid)
